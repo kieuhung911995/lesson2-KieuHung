67e270d64431819c47db14526a3c13ca402ec3ae
kieuhung911995 <kieuhung911995@gmail.com>
2023-08-01 16:09:34 +0700

    1st commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// function openNav(param) {
+//   let overlay = document.getElementsByClassName("header__overlay");
+//   param.classList.toggle("change");
+//   overlay[0].classList.toggle("change");
+//   console.log(overlay[0].classList);
+// }
+// function pageNavigate(x) {
+//   x.classList.add("active");
+// }
+//slider
