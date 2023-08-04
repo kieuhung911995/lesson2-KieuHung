142dbc8891fac109c6721a26f818d84c15ac34f1
kieuhung911995 <kieuhung911995@gmail.com>
2023-08-04 09:29:00 +0700

    update

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-//page
+//open overlay header
+let menu = document.querySelector(".header__bar");
+let overlay = document.querySelector(".header__overlay");
+menu.addEventListener("click", function () {
+  this.classList.toggle("change");
+  overlay.classList.toggle("change");
+});
+
+//check box dropdown
 let filterTitle = document.querySelectorAll(".filter-box__title");
 let filterContent = document.querySelectorAll(
   ".filter-box__title + .filter-box__content"
